4bc6c0d47edab221e50c7868ece67583b7a42d34
pushplaybang <pushplaybang@gmail.com>
2018-10-07 22:10:50 +0200

    first super basic static server in go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	var dir string
+
+	// parse command line args
+	port := flag.String("port","3000", "port to serve HTTP on")
+	path := flag.String("path", "", "path to server")
+	flag.Parse()
+
+	// default to working dir if none is provided
+	if *path == "" {
+		dir, _ = os.Getwd()
+	} else {
+		dir = *path
+	}
+
+	// print to the console
+	fmt.Println("Server started, serving " + *path+ " directory at port " +*port)
+
+	// listen and serve files
+	http.ListenAndServe(":"+ *port, http.FileServer(http.Dir(dir)))
+}
